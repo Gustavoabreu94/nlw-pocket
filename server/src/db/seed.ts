880a461e8513f45a32c852c93c515ce761f70199
Gustavo Abreu <you@example.com>
2024-09-10 12:55:07 -0300

    first class completed

--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -1,2 +1,28 @@
-import {db} from '.'
-import { goalCo}
+import dayjs from "dayjs";
+import { client, db } from ".";
+import { goalCompletion, goals } from "./schema";
+
+async function seed() {
+  await db.delete(goalCompletion);
+  await db.delete(goals);
+
+  const result = await db
+    .insert(goals)
+    .values([
+      { title: "Acorda cedo", desiredWeeklyFrenquency: 5 },
+      { title: "Me exercitar", desiredWeeklyFrenquency: 3 },
+      { title: "Meditar", desiredWeeklyFrenquency: 1 },
+    ])
+    .returning();
+
+  const startOfWeek = dayjs().startOf("week");
+
+  await db.insert(goalCompletion).values([
+    { goalId: result[0].id, createdAt: startOfWeek.toDate() },
+    { goalId: result[1].id, createdAt: startOfWeek.add(1, "day").toDate() },
+  ]);
+}
+
+seed().finally(() => {
+  client.end();
+});
--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,11 +1,67 @@
-import fastify from 'fastify'
+import fastify from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import z from "zod";
+import { createGoal } from "../functions/create-goal";
+import { createGoalCompletion } from "../functions/create-goal-completion";
+import { getWeekPendingGoals } from "../functions/get-week-pending-goals";
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.get("/pending-goals", async () => {
+  const { peddingGoals } = await getWeekPendingGoals();
+
+  return { peddingGoals };
+});
+
+app.post(
+  "/goals",
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrenquency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+  async (request) => {
+    const { title, desiredWeeklyFrenquency } = request.body;
+
+    await createGoal({
+      title,
+      desiredWeeklyFrenquency,
+    });
+  }
+);
+
+app.post(
+  "/completions",
+  {
+    schema: {
+      body: z.object({
+        goalId: z.string(),
+      }),
+    },
+  },
+  async (request) => {
+    const { goalId } = request.body;
+
+    await createGoalCompletion({
+      goalId,
+    });
+  }
+);
 
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    console.log('HTTP server running')
-  })
+    console.log("HTTP server running");
+  });
