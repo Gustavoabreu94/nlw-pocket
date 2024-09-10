9af3a680769cecc7821f045171e3bb7f7e20f125
Gustavo Alves Abreu <gustavoalvesabreu94@gmail.com>
2024-09-09 22:12:00 -0300

    initial commit

---
+++ server/src/db/schema.ts
@@ -0,0 +1,10 @@
+import { integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core'
+
+export const goal = pgTable('goals', {
+  id: text('id').primaryKey(),
+  title: text('title').notNull(),
+  desiredWeeklyFrenquency: integer('desired_weekly_frenquency').notNull(),
+  createdAt: timestamp('created_at', { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+})
